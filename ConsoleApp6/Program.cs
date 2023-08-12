41812679807c1b0eb897d4afbb6a821d6711a642
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-13 01:06:51 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿//System.Linq namespace for more array methods
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numery = { 3, 4, 3, -5, 543, 66, 77, 32, 5, 7, 8, 9, 10, 7, 45 };
+            Console.WriteLine("Max value found in the array: " + numery.Max());
+            Console.WriteLine("Min value found in the array: " + numery.Min());
+            Console.WriteLine("Sum of all the numbers found in the array: " + numery.Sum());
+
+        }
+    }
+}
+
